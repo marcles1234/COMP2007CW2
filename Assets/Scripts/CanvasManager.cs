3447c5b278c78b8a7a0a16c942d991d9d460699a
marcles1234 <marc.ioan55@gmail.com>
2026-04-07 19:11:07 +0100

    Tutorial
    +arch light fixed

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Cinemachine;
 
 public class CanvasManager : MonoBehaviour
@@ -8,6 +9,7 @@ public class CanvasManager : MonoBehaviour
     public GameObject start, game, end;
     public FirstPersonController moveScript;
     public CinemachineVirtualCamera startCam, playerCam;
+    public GameObject startBttn, tutorial;
 
     public void swap()
     {
@@ -24,5 +26,11 @@ public class CanvasManager : MonoBehaviour
             playerCam.Priority = 8;
             end.SetActive(true);
         }
+    }
+
+    public void closeTut()
+    {
+        tutorial.SetActive(false);
+        startBttn.SetActive(true);
     }
 }
